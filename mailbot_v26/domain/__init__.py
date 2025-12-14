6de5a54102b1f130684f63772451a75d29812294
SergioTheFirst <167299076+SergioTheFirst@users.noreply.github.com>
2025-12-14 13:02:40 +0300

    Add domain priority suggestion logging

--- mailbot_v26/domain/__init__.py
+++ mailbot_v26/domain/__init__.py
@@ -1,4 +1,5 @@
 from mailbot_v26.domain.domain_classifier import DomainClassifier, MailTypeClassifier
 from mailbot_v26.domain.domain_policies import DOMAIN_POLICIES
+from mailbot_v26.domain.domain_priority import DOMAIN_PRIORITY_MAP
 
-__all__ = ["DomainClassifier", "MailTypeClassifier", "DOMAIN_POLICIES"]
+__all__ = ["DomainClassifier", "MailTypeClassifier", "DOMAIN_POLICIES", "DOMAIN_PRIORITY_MAP"]
