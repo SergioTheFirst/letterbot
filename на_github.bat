059cf3ed2ade6398419515b2d7048426416ea0ec
Serge <mss777@mail.ru>
2025-12-18 11:45:40 +0300

    auto update from local

--- на_github.bat
+++ на_github.bat
@@ -1,10 +1,13 @@
 @echo off
+set SCRIPT_DIR=%~dp0
 set LOCAL_DIR=C:\pro\mailbot
 
-cd /d "%LOCAL_DIR%"
+cd /d "%LOCAL_DIR%" || exit /b 1
+
+git rev-parse --is-inside-work-tree >nul 2>&1 || exit /b 1
 
 git add -A
 git commit -m "auto update from local"
-git push origin master
+git push origin main
 
 pause
--- с_github.bat
+++ с_github.bat
@@ -1,6 +1,7 @@
 @echo off
-set REPO_URL=https://github.com/SergioTheFirst/mailpro.git
+set SCRIPT_DIR=%~dp0
 set LOCAL_DIR=C:\pro\mailbot
+set REPO_URL=https://github.com/SergioTheFirst/mailpro.git
 
 if exist "%LOCAL_DIR%" (
     rmdir /s /q "%LOCAL_DIR%"
